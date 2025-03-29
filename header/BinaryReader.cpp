a58f6a68f3c8070f23011a6737b6b6e0cd9472d6
NekoMimiOfficial <nekomimi@tilde.team>
2025-03-29 22:41:28 +0300

    finished byteReader class

--- header/BinaryReader.cpp
+++ header/BinaryReader.cpp
@@ -1,5 +1,6 @@
 #include "BinaryReader.hpp"
 #include "outTools.hpp"
+#include <cstddef>
 #include <cstdint>
 #include <vector>
 
@@ -28,4 +29,30 @@ bool byteReader::d()
 {
   if (pointer - 1 < 0) {writer::warn("byteReader", "pointer is at index, can't decrement"); return false;}
   else {pointer--; return true;}
+}
+
+bool byteReader::set(size_t pos)
+{
+  if (pos <= size && pos > 0)
+  {pointer= pos; return true;}
+  else {return false;}
+}
+
+uint8_t byteReader::read()
+{
+  if (pointer > size)
+  {writer::error("byteReader", "reached end of file, can't read more");}
+  uint8_t read= data[pointer];
+  i(1);
+  return read;
+}
+
+uint8_t* byteReader::read(size_t count)
+{
+  uint8_t* buff= new uint8_t[count];
+  for (size_t i= 0; i < count; i++)
+  {
+    buff[i]= read();
+  }
+  return buff;
 }
